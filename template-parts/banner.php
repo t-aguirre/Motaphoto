fec0282ad367cd244831470dee93115cd15294f3
mayi <tala.aguirre@gmail.com>
2025-05-22 19:50:58 +0200

    refactor(home banner): display image via CPT query
    The home banner no longer uses the header image set via the WordPress Customizer. It now displays a random featured image from the 'photos' custom post type, selected via WP_Query.

--- template-parts/banner.php
+++ template-parts/banner.php
@@ -1,8 +1,26 @@
-<!-- Affichage du BANNER avec l'image de background custom -->
+<!-- Dynamically display a background image for the home banner using a random featured image from the "photos" CPT -->
 <?php if (is_front_page() || is_home()) : ?>
-    <?php if (get_header_image()) : ?>
-        <div class="header-banner" style="background-image: url(<?php header_image(); ?>)">
-            <h1 class="header-title"><?php echo get_bloginfo('description'); ?></h1>
-        </div>
-    <?php endif; ?>
+    <?php
+    $args = array(
+        'post_type'      => 'photos',
+        'posts_per_page' => 1,
+        'orderby'        => 'rand',
+    );
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) :
+        while ($query->have_posts()) : $query->the_post();
+            $image_url = wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), array(1440, 962));
+
+            if ($image_url) :
+    ?>
+                <div class="header-banner" style="background-image: url('<?php echo esc_url($image_url[0]); ?>')">
+                    <h1 class="header-title"><?php echo get_bloginfo('description'); ?></h1>
+                </div>
+    <?php
+            endif;
+        endwhile;
+        wp_reset_postdata();
+    endif;
+    ?>
 <?php endif; ?>
